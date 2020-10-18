96ec25d8da6b3c0d437b6e55d39449d1779d7fb0
Ameer Hamza <ameernormie@gmail.com>
2020-10-18 17:08:15 +0500

    chore: use resource picker

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
-import { TitleBar } from "@shopify/app-bridge-react";
+import { useState } from "react";
+import { ResourcePicker, TitleBar } from "@shopify/app-bridge-react";
 import { TextStyle, Page, Layout, EmptyState } from "@shopify/polaris";
 
 const img = "https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg";
@@ -13,27 +14,44 @@ const IndexComp = () => (
   </Page>
 );
 
-const Index = () => (
-  <Page>
-    <TitleBar
-      title="Sample App"
-      primaryAction={{
-        content: "Select products",
-      }}
-    />
-    <Layout>
-      <EmptyState
-        heading="Discount your products temporarily"
-        action={{
+const Index = () => {
+  const [open, setIsOpen] = useState(false);
+
+  function handleSelection(resources) {
+    setIsOpen(false);
+    console.log(resources);
+  }
+
+  return (
+    <Page>
+      <TitleBar
+        title="Sample App"
+        primaryAction={{
           content: "Select products",
-          onAction: () => console.log("clicked"),
+          onAction: () => setIsOpen(true),
         }}
-        image={img}
-      >
-        <p>Select products to change their price temporarily.</p>
-      </EmptyState>
-    </Layout>
-  </Page>
-);
+      />
+      <ResourcePicker
+        resourceType="Product"
+        showVariants={false}
+        open={open}
+        onSelection={(resources) => handleSelection(resources)}
+        onCancel={() => setIsOpen(false)}
+      />
+      <Layout>
+        <EmptyState
+          heading="Discount your products temporarily"
+          action={{
+            content: "Select products",
+            onAction: () => setIsOpen(true),
+          }}
+          image={img}
+        >
+          <p>Select products to change their price temporarily.</p>
+        </EmptyState>
+      </Layout>
+    </Page>
+  );
+};
 
 export default Index;
