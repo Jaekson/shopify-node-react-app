396a8da5b17245d4a4f4f58aeb1fd3ae8d23052c
Ameer Hamza <ameernormie@gmail.com>
2020-10-24 16:33:17 +0500

    chore: add graphql-proxy

--- server.js
+++ server.js
@@ -7,10 +7,14 @@ const {
   verifyRequest,
 } = require("@shopify/koa-shopify-auth");
 const session = require("koa-session");
+const {
+  default: graphQLProxy,
+  ApiVersion,
+} = require("@shopify/koa-shopify-graphql-proxy");
 
 dotenv.config();
 
-const port = parseInt(process.env.PORT, 10) || 3000;
+const port = parseInt(process.env.PORT, 10) || 3001;
 const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const handle = app.getRequestHandler();
@@ -25,7 +29,7 @@ app.prepare().then(() => {
     createShopifyAuth({
       apiKey: SHOPIFY_API_KEY,
       secret: SHOPIFY_API_SECRET_KEY,
-      scopes: ["read_products"],
+      scopes: ["read_products", "write_products"],
       afterAuth(ctx) {
         const { shop, accessToken } = ctx.session;
         ctx.cookies.set("shopOrigin", shop, {
@@ -37,6 +41,8 @@ app.prepare().then(() => {
       },
     })
   );
+  server.use(graphQLProxy({ version: ApiVersion.April20 }));
+
   server.use(verifyRequest());
   server.use(async (ctx) => {
     await handle(ctx.req, ctx.res);
